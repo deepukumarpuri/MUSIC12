ea6f2ecedbceaf7493bdbe54f3213ae77e5c2d54
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-07-24 07:19:05 +0530

    Update channelplay.py

--- VCsMusicBot/modules/channelplay.py
+++ VCsMusicBot/modules/channelplay.py
@@ -396,7 +396,7 @@ async def play(_, message: Message):
                 except Exception:
                     # print(e)
                     await lel.edit(
-                        f"<b>🔴 Flood Wait Error 🔴 \nUser {user.first_name} couldn't join your channel due to heavy requests for userbot! Make sure user is not banned in group."
+                        f"<b>🔴 Flood Wait Error 🔴 \nUser {user.first_name} couldn't join your channel due to heavy requests for userbot! Make sure user is not banned in group. Contact :- @DKBOTZHELP"
                         "\n\nOr manually add assistant to your Group and try again</b>",
                     )
     try:
@@ -663,7 +663,7 @@ async def deezer(client: Client, message_: Message):
                 except Exception:
                     # print(e)
                     await lel.edit(
-                        f"<b>🔴 Flood Wait Error 🔴 \nUser {user.first_name} couldn't join your channel due to heavy requests for userbot! Make sure user is not banned in channel."
+                        f"<b>🔴 Flood Wait Error 🔴 \nUser {user.first_name} couldn't join your channel due to heavy requests for userbot! Make sure user is not banned in channel. Contact :- @DKBOTZHELP"
                         "\n\nOr manually add assistant to your Group and try again</b>",
                     )
     try:
@@ -794,7 +794,7 @@ async def jiosaavn(client: Client, message_: Message):
                     # print(e)
                     await lel.edit(
                         f"<b>🔴 Flood Wait Error 🔴 \nUser {user.first_name} couldn't join your channel due to heavy requests for userbot! Make sure user is not banned in group."
-                        "\n\nOr manually add @VCsMusicPlayer to your Group and try again</b>",
+                        "\n\nOr manually add @DKBOTZHELP to your Group make admin and message bot helper bot join my group</b>",
                     )
     try:
         await USER.get_chat(chid)
