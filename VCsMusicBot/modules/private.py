2d7cd130ae5f88dc0a8c960b789be7470d279203
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-07-24 07:46:21 +0530

    Update private.py

--- VCsMusicBot/modules/private.py
+++ VCsMusicBot/modules/private.py
@@ -22,7 +22,7 @@ def _start(client, message):
                         "Channel 📢", url=f"https://t.me/{UPDATES_CHANNEL}")
                 ],[
                     InlineKeyboardButton(
-                        "🔥 Source Code 🔥", url=f"https://{SOURCE_CODE}")
+                        "🔥 OWNER 🔥", url=f"https://t.me/{SOURCE_CODE}")
                 ]
             ]
         ),
