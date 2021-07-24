53096c4deaaab69a4d0614b4ec17f29e97690501
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-07-24 07:27:43 +0530

    Update msg.py

--- VCsMusicBot/modules/msg.py
+++ VCsMusicBot/modules/msg.py
@@ -1,7 +1,7 @@
 import os
 from VCsMusicBot.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL
 class Messages():
-      START_MSG = "**Hello 👋 [{}](tg://user?id={})!**\n\n🤖 I am an advanced bot created for playing music in the voice chats of Telegram Groups & Channels.\n\n✅ Hits /help for more info."
+      START_MSG = "**Hello 👋 [{}](tg://user?id={})!**\n\n🤖 I am an advanced bot created for playing music in the voice chats of Telegram Groups & Channels.\n\n✅ Hits /help for more info.Watch this bot to know how to play song in group link :- https://www.youtube.com/watch?v=bT-l-aczXUg"
       HELP_MSG = [
         ".",
 f"""
@@ -103,3 +103,4 @@ f"""
 **Sudo Users can execute any command in any groups.**
 """
       ]
+TUTORIAL_MSG = "**WATCH THIS VIDEO TO KNOW HOW TO PLAY SONG LINK :- https://www.youtube.com/watch?v=bT-l-aczXUg  HOW TO RESUME PAUSE NEXT SONG PLAY VIDEO LINK :- https://www.youtube.com/watch?v=3rxoVAwXqtM"
