c8cca016802d144410ee950fb78655d296c8e4bd
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-12-13 08:02:21 +0530

    Update broadcast.py

--- etc/broadcast.py
+++ etc/broadcast.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 import datetime
 import time
 from database.users_chats_db import db
-from DKMusic.config import ADMINS
+from VCsMusicBot.config import ADMINS
 from utils import broadcast_messages
 import asyncio
         
